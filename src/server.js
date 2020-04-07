fc04f3164b7b5e87b4199fff1af823a380a65fc4
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-04-07 18:01:53 -0300

    implement: server basic functions

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const nunjucks = require("nunjucks");
+
+const server = express();
+
+server.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST",
+    optionsSuccessStatus: 204,
+  })
+); // 👁 I'll configure this after dev
+
+server.use(express.static(__dirname + "/public"));
+
+nunjucks.configure(__dirname + "/views", {
+  express: server,
+  noCache: true, // 👁 I'll set this to false after dev
+});
+
+server.use(routes);
+
+server.listen(3333);
+console.log(`👁  SERVER ♦ ••RUNNING•• ♦ PID: ${process.pid} ♦`);
