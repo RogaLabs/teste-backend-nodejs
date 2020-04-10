f8db91ea422fb4d148ca7b2e88e22d635865ea24
Nikolo Iosifovich <vieiraneto88@gmail.com>
2020-04-10 06:21:57 -0300

    change: refactoring routes

--- src/routes.js
+++ src/routes.js
@@ -1,59 +1,11 @@
 const express = require("express");
-const axios = require("axios").default;
-
+const geoApi = require("./utils/geoApi");
 const IncidentController = require("./controllers/IncidentController");
-require("dotenv").config();
 
 const routes = express.Router();
 
 routes.use(async (req, res, next) => {
-  const { latitude: lat, longitude: lng } = req.body;
-  const data = req.body;
-
-  const errors = await axios
-    .get("http://www.mapquestapi.com/geocoding/v1/reverse", {
-      params: {
-        key: `${process.env.API_KEY}`,
-        location: `${lat},${lng}`,
-        outFormat: "json",
-        thumbMaps: "false",
-      },
-    })
-    .then(function (response) {
-      const { statuscode } = response.data.info;
-      const location = response.data.results[0].locations[0];
-      const isStreet =
-        location != undefined && location.geocodeQuality == "STREET"
-          ? true
-          : false;
-
-      if (statuscode < 400 && location != undefined && isStreet) {
-        data.endereco = {
-          logradouro: location.street,
-          bairro: location.adminArea6,
-          cidade: location.adminArea5,
-          estado: location.adminArea3,
-          pais: location.adminArea1,
-          cep: location.postalCode,
-        };
-        req.body = { data: data };
-        return req.body;
-      } else if (statuscode >= 400) {
-        return (response.status = 400);
-      } else if (location == undefined) {
-        return (response.status = 404);
-      } else if (!isStreet) {
-        return (response.status = 404);
-      } else {
-        return response;
-      }
-    })
-    .catch(function (error) {
-      if (error.response.status >= 400) {
-        return error.response.status;
-      }
-    });
-  res.body = errors;
+  await geoApi(req, res);
   next();
 });
 
