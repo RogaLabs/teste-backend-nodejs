0529c6d08901e73c0f203b26b24ffca7a14491ea
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-19 16:44:31 -0300

    Add complaint service tests suit

--- tests/fixtures/complaint/complaint-fixture.js
+++ tests/fixtures/complaint/complaint-fixture.js
@@ -2,7 +2,7 @@
 	function () {
 		'use-strict'
 		module.exports = {
-			complaint: require('./complaint.json'),
+			complaint: require('./complaint'),
 			newComplaint: require('./new-complaint'),
 			createdComplaint: require('./created-complaint'),
 			modifiedComplaint: require('./modified-complaint'),
---
+++ tests/unit/complaint/complaint.service.spec.js
@@ -0,0 +1,21 @@
+const chai = require('chai')
+const expect = chai.expect
+const sinon = require('sinon')
+
+const mongoose = require('mongoose')
+
+const ComplaintModule = require('../../../modules/complaint/complaint.module')
+const ComplaintModel = ComplaintModule.ComplaintModel
+const ComplaintService = ComplaintModule.ComplaintService
+
+const Fixtures = require('../../fixtures/fixtures')
+const ComplaintFixture = Fixtures.ComplaintFixture
+const ErrorFixture = Fixtures.ErrorFixture
+
+var ComplaintModelMock
+
+describe('ComplaintService', () => {
+	beforeEach(() => {
+		ComplaintModelMock = sinon.mock(ComplaintModel)
+	})
+})
