0aae2da59bb61c7d56e6e6bc22d88c9f7e5cfa3c
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-19 14:11:38 -0300

    Add prepareConnection function

--- modules/mongodb/mongodb.util.js
+++ modules/mongodb/mongodb.util.js
@@ -4,8 +4,24 @@
 	module.exports = {
 		init: init
 	};
+	// Conexão mongodb
+	const mongoose = require('mongoose')
+	const mongodbConfig = require('../../config/mongodb/mongodb-config.json').mongodb
+
+	function prepareConnectionString(config) {
+		var connectionString = 'mongodb://';
+
+		if (config.user) {
+				connectionString += config.user + ':' + config.password + '@';
+		}
+
+		connectionString += config.server + '/' + config.database;
+
+		return connectionString;
+	}
+
 
 	function init(){
-		
+
 	}
 })();
