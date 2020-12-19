201d91c8ce3f26de8699c76aa0a26e20e83322ba
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-19 11:55:07 -0300

    Add test function init of mongodb.util

--- modules/mongodb/mongodb.util.js
+++ modules/mongodb/mongodb.util.js
@@ -2,6 +2,10 @@
 	'use strict';
 
 	module.exports = {
+		init: init
 	};
 
+	function init(){
+		
+	}
 })();
--- tests/unit/mongodb.util.specs.js
+++ tests/unit/mongodb.util.specs.js
@@ -7,4 +7,7 @@ describe('Arquivo mongodb.util', () => {
 	it('deve confirmar que MondoDB.module retorna MongoDBUtil', () => {
 		expect(MongoDBUtil).to.be.a('object')
 	})
+	it('deve confirmar se a função init existe', () => {
+		expect(MongoDBUtil.init).to.be.a('function')
+	})
 })
