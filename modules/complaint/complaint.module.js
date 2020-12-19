d8f276ca509a0016de1c91459cbd37039f95893f
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-19 15:30:45 -0300

    Complaint module return object test passing

--- modules/complaint/complaint.module.js
+++ modules/complaint/complaint.module.js
@@ -4,7 +4,7 @@
 		module.exports = init
 
 		function init() {
-			
+			return {}
 		}
 	}
 )()
--- tests/unit/complaint/complaint.module.spec.js
+++ tests/unit/complaint/complaint.module.spec.js
@@ -7,4 +7,8 @@ describe('ComplaintModule', function()  {
 	it('deve confirmar se a função ComplaintModule existe', function() {
 		expect(ComplaintModule).to.be.a('function')
 	})
+	
+	it('deve confirmar se a função ComplaintModule retorna um objeto', function() {
+		expect(ComplaintModule()).to.be.a('object')
+	})
 })
