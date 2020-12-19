37de7a6a4e266a0a0f278a4c48c9ff50a7e785c7
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-19 11:46:34 -0300

    Refactor mongodb.utils.spec test

--- tests/unit/mongodb.util.specs.js
+++ tests/unit/mongodb.util.specs.js
@@ -1,10 +1,10 @@
 var chai = require('chai')
 var expect = chai.expect
-var MongoDBModule = require('../../modules/mongodb/mongodb.module')
+var MongoDBUtil = require('../../modules/mongodb/mongodb.module').MongoDBUtil
 
 // usando o mocha
 describe('Arquivo mongodb.util', () => {
 	it('deve confirmar que o arquivo mongodb.util existe', () => {
-		expect(MongoDBModule.MongoDBUtil).to.be.a('object')
+		expect(MongoDBUtil).to.be.a('object')
 	})
 })
