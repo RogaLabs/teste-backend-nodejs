bac1864ce96368cb2907723451c9c7fb7c758fd4
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-19 15:35:11 -0300

    Add ComplaintController test

--- tests/unit/complaint/complaint.module.spec.js
+++ tests/unit/complaint/complaint.module.spec.js
@@ -3,12 +3,16 @@ const expect = chai.expect
 
 const ComplaintModule = require('../../../modules/complaint/complaint.module')
 
-describe('ComplaintModule', function()  {
-	it('deve confirmar se a função ComplaintModule existe', function() {
+describe('ComplaintModule', () => {
+	it('deve confirmar se a função ComplaintModule existe', () => {
 		expect(ComplaintModule).to.be.a('function')
 	})
-	
-	it('deve confirmar se a função ComplaintModule retorna um objeto', function() {
+
+	it('deve confirmar se a função ComplaintModule retorna um objeto', () => {
 		expect(ComplaintModule()).to.be.a('object')
+	})
+
+	it('deve confirmar se a função ComplaintController xiste', () => {
+		expect(ComplaintModule().ComplaintController, to.be.a('function'))
 	})
 })
