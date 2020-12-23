1c879fd1d6b02e968653551c1b4c6d9c4ff22d0b
Fabio Santos <fabioprogramadorti@gmail.com>
2020-12-23 10:45:58 -0300

    Load all modules for running middleware tests

--- tests/unit/complaint/complaint.middleware.spec.js
+++ tests/unit/complaint/complaint.middleware.spec.js
@@ -3,3 +3,15 @@ const expect = chai.expect
 const sinon = require('sinon')
 
 const httpMocks = require('node-mocks-http')
+
+const Promisse = require('bluebird').Promise
+
+const ComplaintModule = require('../../../modules/complaint/complaint.module')()
+const ComplaintMiddleware = ComplaintModule.ComplaintMiddleware
+const ComplaintService = ComplaintModule.ComplaintService
+
+const Fixtures = require('../../fixtures/fixtures')
+const ComplaintFixture = Fixtures.ComplaintFixture
+const ErrorFixture = Fixtures.ErrorFixture
+
+var req, res, next
